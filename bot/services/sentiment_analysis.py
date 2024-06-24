2affd8a0bab853609f1a6443464471441edd6e06
Elizaveta Pernikova <lisapernikowa@mail.ru>
2024-06-25 01:15:43 +0500

    add YoutubeParser and OllamaChat for work with LM

--- bot/services/sentiment_analysis.py
+++ bot/services/sentiment_analysis.py
@@ -1,1 +1,85 @@
 #Модуль для анализа тональности комментариев
+import ollama
+import re
+from youtube_service import YoutubeParser
+class OllamaChat:
+    parser = YoutubeParser()
+    def __init__(self, model_name='llama3'):
+        self.model_name = model_name
+    def get_characteristics(self, url) -> str:
+        match = re.search(r"v=([a-zA-Z0-9_-]+)", url)
+        if match:
+            url = match.group(1)
+        #else
+        comments = self.parser.get_video_comments(url)
+        with open("q_characteristics.txt", 'r', encoding='utf-8') as file:
+            base_prompt = file.read()
+
+        prompt = "\n".join(comments) + "\n" + base_prompt
+
+        stream = ollama.chat(model=self.model_name,
+                             messages=[{'role': 'user', 'content': prompt}],
+                             stream=True)
+
+        response_parts = []
+        for chunk in stream:
+            response_parts.append(chunk['message']['content'])
+
+        response_text = "".join(response_parts)
+        json_start = response_text.find("[")
+        json_end = response_text.find("]")
+        if json_start != -1 and json_end != -1:
+            return response_text[json_start:json_end + 1]
+        else:
+            return response_text
+
+    def get_sentiment(self, url) -> str:
+        match = re.search(r"v=([a-zA-Z0-9_-]+)", url)
+        if match:
+            url = match.group(1)
+        #else
+        comments = self.parser.get_video_comments(url)
+        with open("q_tonality.txt", 'r', encoding='utf-8') as file:
+            base_prompt = file.read()
+
+        prompt = "\n".join(comments) + "\n" + base_prompt
+
+        stream = ollama.chat(model=self.model_name,
+                             messages=[{'role': 'user', 'content': prompt}],
+                             stream=True)
+
+        response_parts = []
+        for chunk in stream:
+            response_parts.append(chunk['message']['content'])
+
+        response_text = "".join(response_parts)
+        json_start = response_text.find("[")
+        json_end = response_text.find("]")
+        if json_start != -1 and json_end != -1:
+            return response_text[json_start:json_end + 1]
+        else:
+            return response_text
+
+    def get_summary(self, url) -> str:
+        match = re.search(r"v=([a-zA-Z0-9_-]+)", url)
+        if match:
+            url = match.group(1)
+        #else
+        comments = self.parser.get_subtitles(url)
+        with open("q_summary.txt", 'r', encoding='utf-8') as file:
+            base_prompt = file.read()
+
+        prompt = "\n".join(comments) + "\n" + base_prompt
+
+        stream = ollama.chat(model=self.model_name,
+                             messages=[{'role': 'user', 'content': prompt}],
+                             stream=True)
+
+        response_parts = []
+        for chunk in stream:
+            response_parts.append(chunk['message']['content'])
+
+        response_text = "".join(response_parts)
+        return response_text
+
+
--- bot/services/youtube_service.py
+++ bot/services/youtube_service.py
@@ -1,1 +1,59 @@
 #Сервис для работы с API YouTube
+from googleapiclient.discovery import build
+import requests
+from sqlalchemy import null
+
+
+class YoutubeParser:
+    def __init__(self):
+        self.YT_api_key = 'AIzaSyCGros2SeZwb4utjrTfL2Hcbuc2kx4FxJI'
+        self.SA_api_key = 'fWG6yD8V8C9aUjJaXrpGnT5b'
+        self.comments = []
+
+    #def get_gen_inf(self, video_id):
+    def get_video_comments(self, video_id):
+        youtube = build('youtube', 'v3', developerKey=self.YT_api_key)
+        video_response = youtube.commentThreads().list(
+            part='snippet,replies',
+            videoId=video_id
+        ).execute()
+
+        while video_response:
+            for item in video_response['items']:
+                comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+                self.comments.append('"' + comment + '"')
+
+                replycount = item['snippet']['totalReplyCount']
+                if replycount > 0:
+                    self.comments.append("ответы на комментарий: [ ")
+                    for reply in item['replies']['comments']:
+                        reply = reply['snippet']['textDisplay']
+                        self.comments.append('"' + reply + '"')
+                    self.comments.append(" ]")
+
+            if 'nextPageToken' in video_response:
+                video_response = youtube.commentThreads().list(
+                    part='snippet,replies',
+                    videoId=video_id,
+                    pageToken=video_response['nextPageToken']
+                ).execute()
+            else:
+                break
+        return self.comments
+
+    def get_subtitles(self, video_id):
+        url = "https://www.searchapi.io/api/v1/search"
+        params = {
+            "api_key": self.SA_api_key,
+            "engine": "youtube_transcripts",
+            "video_id": video_id,
+            "lang": "ru"  # проблема если субтитры на английском вернет ошибку
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        if "transcripts" in data:
+            return [item["text"] for item in data.get("transcripts", []) if "text" in item]
+        else:
+            return null
+
+
