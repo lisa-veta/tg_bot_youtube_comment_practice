953a9d21dd8d3985f9161ff16066220e40db0d56
Elizaveta Pernikova <lisapernikowa@mail.ru>
2024-06-26 20:56:57 +0500

    improved YoutubeParser and OllamaChat

--- bot/services/youtube_service.py
+++ bot/services/youtube_service.py
@@ -1,7 +1,6 @@
 #Сервис для работы с API YouTube
 from googleapiclient.discovery import build
 import requests
-from sqlalchemy import null
 
 
 class YoutubeParser:
@@ -10,14 +9,40 @@ class YoutubeParser:
         self.SA_api_key = 'fWG6yD8V8C9aUjJaXrpGnT5b'
         self.comments = []
 
-    #def get_gen_inf(self, video_id):
+    def get_general_inf(self, video_id):
+        url = f"https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&id={video_id}&key={self.YT_api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            stats = data['items'][0]['statistics']
+            del stats['favoriteCount']
+            snippet = data['items'][0]['snippet']
+            title = snippet['title']
+            published_at = snippet['publishedAt']
+            return {
+                'title': title,
+                'published_at': published_at,
+                'viewCount': stats['viewCount'],
+                'likeCount': stats['likeCount'],
+                'commentCount': stats['commentCount']
+            }
+        else:
+            print(f"Ошибка: {response.status_code} - {response.text}")
+            return None
+
+    def get_comment_count(self, video_id):
+        general_info = self.get_general_inf(video_id)
+        if general_info:
+            return general_info['commentCount']
+        else:
+            return None
+
     def get_video_comments(self, video_id):
         youtube = build('youtube', 'v3', developerKey=self.YT_api_key)
         video_response = youtube.commentThreads().list(
             part='snippet,replies',
             videoId=video_id
         ).execute()
-
         while video_response:
             for item in video_response['items']:
                 comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
@@ -39,6 +64,9 @@ class YoutubeParser:
                 ).execute()
             else:
                 break
+        comment_count = self.get_comment_count(video_id)
+        if comment_count:
+            self.comments.append("Количество всех комментариев: " + str(comment_count))
         return self.comments
 
     def get_subtitles(self, video_id):
@@ -54,6 +82,6 @@ class YoutubeParser:
         if "transcripts" in data:
             return [item["text"] for item in data.get("transcripts", []) if "text" in item]
         else:
-            return null
+            return None
 
 
